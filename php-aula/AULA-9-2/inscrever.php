e4ff3ea32815fd4a298ed0e17d3b309c223aa1b9
Thiago <47253697+ThiagoP08@users.noreply.github.com>
2022-10-28 21:03:22 -0300

    Add files via upload

---
+++ php-aula/AULA-9-2/inscrever.php
@@ -0,0 +1,35 @@
+<div>
+    <form action="">
+
+    <div>
+        <div>
+            <label for="">
+                Nome: </br>
+                <input type="text" name="nome" >
+            </label>
+        </div>
+
+        <div>
+            <label for="">
+                E-mail: </br>
+                <input type="email" name="email">
+            </label>
+        </div>
+
+        <div>
+            <label for="">
+                Senha: </br>
+                <input type="password" name="password">
+            </label>
+        </div>
+        
+        <div>
+            <label for="">
+                Confirmar a Senha: </br>
+                <input type="password" name="password_confirm">
+            </label>
+        </div>
+    </div>
+
+    </form>
+</div>
